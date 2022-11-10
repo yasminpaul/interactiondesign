e516fad261bc4f7f6baa8abb041679838a7f7d83
yazyp <yasminfpaul@gmail.com>
2022-11-10 17:26:40 +0000

    navigation component complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import Week1 from './components/Week1';
+import Week2 from './components/Week2';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () => (
+  <nav>
+        <h1> Welcome to Interaction Design!</h1>
+        <ul>
+          <li>
+            <Link to ="/week1">
+              {" "}
+              <h1> Week 1</h1></Link>
+            <Link to ="/week2"> <h1>Week 2</h1></Link>
+          </li>
+        </ul>
+      </nav>
+);
 
 const App = () => (
   <>
